b5a9912bf69c7f39af9e6926ce8a714586f38761
Ammar Hasan <ammar.hasan37@qmail.cuny.edu>
2025-05-12 19:22:34 -0400

    added styling

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,9 @@
 export default function Home() {
   return (
     <>
-<h1>Final Exam 343</h1>
-<h2>Ammar Hasan</h2>
-<h3>Spring 2025</h3>
+      <h1 className="text-4xl font-bold text-blue-700 mb-4">Final Exam 343</h1>
+      <h2 className="text-2xl font-semibold text-gray-800 mb-2">Ammar Hasan</h2>
+      <h3 className="text-xl font-medium text-gray-600">Spring 2025</h3>
     </>
-
   );
 }
